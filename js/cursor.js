69b30a80029a3b4750df65209559883a6cada32f
Jay Nash <jn202871@gmail.com>
2023-04-18 20:01:40 -0400

    init

---
+++ js/cursor.js
@@ -0,0 +1,8 @@
+let cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+});
