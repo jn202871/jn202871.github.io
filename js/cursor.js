4b0549fd9740b7a25c8d021325a44f534a67a06c
Jay Nash <jn202871@gmail.com>
2023-04-23 17:36:02 -0400

    Reformat

--- js/cursor.js
+++
@@ -1,8 +0,0 @@
-let cursor = document.getElementById("cursor");
-
-document.addEventListener("mousemove", (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
-  cursor.style.left = x + "px";
-  cursor.style.top = y + "px";
-});
