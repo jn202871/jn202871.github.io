6a8ed8c27f979ddda7bf1402761bd7df44b3c9ce
Jay Nash <jn202871@gmail.com>
2023-05-23 14:02:55 -0400

    Blog Post

--- js/blog.js
+++ js/blog.js
@@ -49,3 +49,5 @@ window.matchMedia("(max-width: 800px)").onchange = e => {
 
   nav.dataset.toggled = "false";
 };
+
+let myLabels = document.querySelectorAll('.lbl-toggle'); Array.from(myLabels).forEach(label => { label.addEventListener('keydown', e => { if (e.which === 32 || e.which === 13) { e.preventDefault(); label.click(); }; }); }); 
